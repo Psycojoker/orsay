3fa7a4a38294eefaa7aef1e3e470418443abc887
Laurent Peuch <cortex@worlddomination.be>
2012-11-08 23:20:17 +0100

    init

---
+++ quai/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Contact(models.Model):
+    is_called = models.CharField(max_length=255)
+    in_recontacting_loop = models.BooleanField(default=True)
+    notes = models.TextField(null=True, blank=True)
+
+
+class Meet(models.Model):
+    place = models.CharField(max_length=255)
+    contact = models.ForeignKey(Contact)
+    description = models.TextField(null=True, blank=True)
+    datetime = models.DateTimeField()
