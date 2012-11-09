89c70cae0925ae7b8189a35be89c6e9599a4f9ed
Laurent Peuch <cortex@worlddomination.be>
2012-11-09 07:18:22 +0100

    [enh] form in modal to add a new person

--- quai/views.py
+++ quai/views.py
@@ -1,6 +1,17 @@
+from django import forms
 from django.shortcuts import render
 
 from models import Person
 
+
+class PersonForm(forms.ModelForm):
+    class Meta:
+        model = Person
+
+
 def home(request):
-    return render(request, "quai/person_list.html", {"persons": Person.objects.all()})
+    form = PersonForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        form.save()
+
+    return render(request, "quai/person_list.html", {"persons": Person.objects.all(), "person_form": form})
